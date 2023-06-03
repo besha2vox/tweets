4a6410512540664cbafb7e0a80cc97777255e097
Serhii Revva <besha2vox@gmail.com>
2023-06-03 09:42:11 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      Tweets
+    </div>
+  );
+};
+
+export default App;
